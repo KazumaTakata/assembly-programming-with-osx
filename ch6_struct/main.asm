999b78d4188f3cc812effdd76426308d3662a312
Kazuma Takata <neilbuzztakata@gmail.com>
2020-02-28 14:31:54 +0900

    struct

--- ch6_struct/main.asm
+++ ch6_struct/main.asm
@@ -1,12 +1,20 @@
     global  _main
     extern  _printf
+    extern  _malloc
     default  rel
+
+
+    section .data
+    struc Token
+.type resq  1
+.value resq 1
+    endstruc
+
 
     section .text
 
-message: db "hello world 10 times", 10, 0
 
-string: db "(+ 3 3)", 0
+string: db "sample string", 0
 
 i equ 0
 
@@ -15,27 +23,38 @@ _main:
     mov  rbp, rsp
     sub  rsp, 16
 
-    mov  al, byte [string]
 
-    mov  rax, 10
-    mov  qword [rsp+i], rax
-
-_loop:
-
-    lea  rdi, [message]
-    xor  rax, rax
-    call  _printf
-
-
-    mov rax, qword [rsp+i]
-
-    cmp rax, 0
-    sub rax, 1 
-    mov qword [rsp+i], rax
-
-    jne _loop
+    mov rdi, 0
+    lea rsi, [string]
+    call _init_Token
 
 
     xor  rax, rax
     leave
     ret
+
+
+_init_Token:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 16 
+
+    mov [rsp + 0], rdi
+    mov [rsp + 8], rsi
+
+
+    mov rdi, Token_size
+    call _malloc
+
+    mov rcx, [rsp + 0]
+    mov qword [rax + Token.type], rcx
+
+    mov rcx, [rsp + 8]
+    mov qword [rax + Token.value], rcx
+
+
+
+
+    leave
+    ret
+        
